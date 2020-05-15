0fce1ac81ad27194f2d86b52c8482bdcb1cc5371
Markus Fleschutz <markus@fleschutz.de>
2020-05-15 18:05:59 +0000

    Added more functions

--- EDP.c
+++ EDP.c
@@ -3,6 +3,23 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+typedef enum {
+	PRE_RUN, RUN, POST_RUN,
+	PRE_EXIT, EXIT, POST_EXIT,
+} Type;
+static void (*calls[100])(void);
+
+static void remember(void (*func)(void), Type type)
+{
+	calls[type] = func;
+}
+
+static void call(Type type)
+{
+	if (calls[type])
+		(*calls[type])();
+}
+
 void at_pre_sunrise(void (*func)(void))
 {
 	// TODO
@@ -33,22 +50,50 @@ void at_noon(void (*func)(void))
 	// TODO
 }
 
+void at_pre_run(void (*func)(void))
+{
+	remember(func, PRE_RUN);
+}
+
+void at_run(void (*func)(void))
+{
+	remember(func, RUN);
+}
+
+void at_post_run(void (*func)(void))
+{
+	remember(func, POST_RUN);
+}
+
+static void _on_exit(void)
+{
+	call(PRE_EXIT);
+	call(EXIT);
+	call(POST_EXIT);
+}
+
 void at_pre_exit(void (*func)(void))
 {
-	atexit(func);
+	atexit(_on_exit);
+	remember(func, PRE_EXIT);
 }
 
 void at_exit(void (*func)(void))
 {
-	atexit(func);
+	atexit(_on_exit);
+	remember(func, EXIT);
 }
 
 void at_post_exit(void (*func)(void))
 {
-	atexit(func);
+	atexit(_on_exit);
+	remember(func, POST_EXIT);
 }
 
 void run()
 {
+	call(PRE_RUN);
+	call(RUN);
 	usleep(5 * 1000 * 1000);
+	call(POST_RUN);
 }
--- main.c
+++ main.c
@@ -26,6 +26,16 @@ void on_exit()
 	puts("good-bye");
 }
 
+void on_pre_run()
+{
+	puts("entering run()");
+}
+
+void on_post_run()
+{
+	puts("leaving run()");
+}
+
 void main()
 {
 	at_sunrise(on_sunrise);
@@ -33,5 +43,7 @@ void main()
 	at_sunset(on_sunset);
 	at_noon(on_noon);
 	at_exit(on_exit);
+	at_pre_run(on_pre_run);
+	at_post_run(on_post_run);
 	run();
 }
